3cd97f60759faa1877be53522f497e129c0edbd8
ODooHo <engh0205@naver.com>
2025-02-01 13:41:59 +0900

    initial: project Setting
    * spotless 의존성 추가
    * 프로젝트 명 반영

---
+++ src/main/java/com/stamp/controller/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.stamp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+
+@RequestMapping("/api/v1")
+@RestController
+public class HealthCheckController {
+    @GetMapping("/healthCheck")
+    public String healthCheck() {
+        LocalDateTime now = LocalDateTime.now();
+        return "server is Up! requested at:" + now;
+    }
+}
