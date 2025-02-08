6c0e4718ccb98c602107f018d07d90eaac554498
ODooHo <engh0205@naver.com>
2025-02-08 16:17:54 +0900

    feat(auth-3): end-to-end 테스트를 위한 테스트 컨트롤러 추가
    - 임의로 만든 프론트엔드 화면 -> 백엔드 인증 후 멤버 생성 간의 여정을 테스트하기 위해 만든 컨트롤러 클래스(헬스 체크에 이어서)

--- src/main/java/com/stamp/api/controller/HealthCheckController.java
+++ src/main/java/com/stamp/api/controller/HealthCheckController.java
@@ -1,16 +1,36 @@
 package com.stamp.api.controller;
 
+import com.stamp.api.auth.domain.EmployerUser;
+import com.stamp.api.auth.repository.EmployerUserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.time.LocalDateTime;
 
+@Slf4j
+@RequiredArgsConstructor
 @RequestMapping("/api/v1")
 @RestController
 public class HealthCheckController {
+
+    private final EmployerUserRepository employerUserRepository;
+
     @GetMapping("/healthCheck")
     public String healthCheck() {
+        LocalDateTime now = LocalDateTime.now();
+        return "server is Up! requested at:" + now;
+    }
+
+    @GetMapping("/authenticated-healthCheck")
+    public String authenticatedHealthCheck(@AuthenticationPrincipal UserDetails user) {
+        log.info(user.toString());
+        EmployerUser employerUser = employerUserRepository.findById(Long.valueOf(user.getUsername())).orElseGet(null);
+        log.info(String.valueOf(employerUser));
         LocalDateTime now = LocalDateTime.now();
         return "server is Up! requested at:" + now;
     }
