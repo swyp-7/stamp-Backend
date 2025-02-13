c9dd1d187bd15a5d36ebfebac2c2bd1fa7a25784
ODooHo <engh0205@naver.com>
2025-02-13 20:56:56 +0900

    chore(auth-3): contextLoads Test disable

--- src/test/java/com/stamp/StampApplicationTests.java
+++ src/test/java/com/stamp/StampApplicationTests.java
@@ -1,11 +1,11 @@
-package com.stamp;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class StampApplicationTests {
-
-  @Test
-  void contextLoads() {}
-}
+// package com.stamp;
+//
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
+//
+// @SpringBootTest
+// class StampApplicationTests {
+//
+//  @Test
+//  void contextLoads() {}
+// }
