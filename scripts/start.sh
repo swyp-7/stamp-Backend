7c9a6fc3fffc82a602d4eb5154aab49c06bc2612
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-02-18 00:46:47 +0900

    chore: fix CI/CD script

--- scripts/start.sh
+++ scripts/start.sh
@@ -22,7 +22,7 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -Djasypt.encryptor.password="JASYPT_SECRET" -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -Djasypt.encryptor.password=$JASYPT_SECRET -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 프로세스 실행완료. [PID: $CURRENT_PID]" >> $DEPLOY_LOG
