0565d628d5bcb5630227dd01b6b7d958ede58424
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-02-18 00:46:47 +0900

    fix: CI/CD pipeline - EC2에 옮길 파일을 제한 - EC2 배포 로그 수정

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,12 +9,15 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
-if [ -f .env ]; then
-  source .env
+if [ -f $PROJECT_ROOT/scripts/.env ]; then
+  source $PROJECT_ROOT/scripts/.env
 else
-  echo "환경 변수 파일을 찾을 수 없습니다." >> $ERROR_LOG
+  echo "$TIME_NOW > 환경 변수 파일을 찾을 수 없습니다." >> $ERROR_LOG
+  echo "현재 디렉토리: $(pwd)" >> $ERROR_LOG
+  ls -al >> $ERROR_LOG
   exit 1
 fi
+
 
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
@@ -22,7 +25,8 @@ cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -Djasypt.encryptor.password="JASYPT_SECRET" -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+echo "Run command: java -D$JASYPT_SECRET -jar $JAR_FILE" >> $DEPLOY_LOG
+nohup java -D$JASYPT_SECRET -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
 echo "$TIME_NOW > 프로세스 실행완료. [PID: $CURRENT_PID]" >> $DEPLOY_LOG
