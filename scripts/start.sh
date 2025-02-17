5f6f5a136b82409664019b4d1ac23c25a6a43e42
SnoopyComp <hyunchang52@gmail.com>
2025-02-17 14:50:18 +0900

    fix: CICD에 jasypt decryption 추가

--- scripts/start.sh
+++ scripts/start.sh
@@ -9,13 +9,20 @@ DEPLOY_LOG="$PROJECT_ROOT/deploy.log"
 
 TIME_NOW=$(date +%c)
 
+if [ -f .env ]; then
+  source .env
+else
+  echo "환경 변수 파일을 찾을 수 없습니다." >> $ERROR_LOG
+  exit 1
+fi
+
 # build 파일 복사
 echo "$TIME_NOW > $JAR_FILE 파일 복사" >> $DEPLOY_LOG
 cp $PROJECT_ROOT/build/libs/*.jar $JAR_FILE
 
 # jar 파일 실행
 echo "$TIME_NOW > $JAR_FILE 파일 실행" >> $DEPLOY_LOG
-nohup java -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
+nohup java -Djasypt.encryptor.password="JASYPT_SECRET" -jar $JAR_FILE > $APP_LOG 2> $ERROR_LOG &
 
 CURRENT_PID=$(pgrep -f $JAR_FILE)
-echo "$TIME_NOW > 프로세스 실행완료. [PID: $CURRENT_PID]" >> $DEPLOY_LOG
+echo "$TIME_NOW > 프로세스 실행완료. [PID: $CURRENT_PID]" >> $DEPL OY_LOG
