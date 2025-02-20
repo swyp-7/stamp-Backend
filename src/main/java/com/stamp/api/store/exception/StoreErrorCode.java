a9cafea9b3a68f8325a8e30a50a7b2e95a8ead72
ODooHo <engh0205@naver.com>
2025-02-20 19:06:03 +0900

    feat(store-26): 주소 - 기본 주소 및 상세 주소로 분리

--- src/main/java/com/stamp/api/store/exception/StoreErrorCode.java
+++ src/main/java/com/stamp/api/store/exception/StoreErrorCode.java
@@ -6,18 +6,19 @@ import org.springframework.http.HttpStatus;
 
 @RequiredArgsConstructor
 public enum StoreErrorCode implements Error {
-  BUSINESS_NUMBER_ALREADY_EXISTED(HttpStatus.BAD_REQUEST, "이미 존재하는 사업자 번호 입니다.");
+  BUSINESS_NUMBER_ALREADY_EXISTED(HttpStatus.BAD_REQUEST, "이미 존재하는 사업자 번호 입니다."),
+  STORE_NOT_FOUNDED(HttpStatus.NOT_FOUND, "존재하지 않는 가게입니다.");
 
   private final HttpStatus status;
   private final String message;
 
   @Override
   public HttpStatus getStatus() {
-    return null;
+    return status;
   }
 
   @Override
   public String getMessage() {
-    return "";
+    return message;
   }
 }
--- src/main/java/com/stamp/api/store/repository/StoreRepository.java
+++ src/main/java/com/stamp/api/store/repository/StoreRepository.java
@@ -3,7 +3,13 @@ package com.stamp.api.store.repository;
 import com.stamp.api.store.entity.Store;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface StoreRepository extends JpaRepository<Store, Long> {
   Optional<Store> findByBusinessNumber(String businessNumber);
+
+  @Query(
+      "SELECT s FROM Store s LEFT JOIN FETCH s.storeScheduleList WHERE s.id = :storeId AND s.deletedAt IS NULL")
+  Optional<Store> findByIdWithSchedules(@Param("storeId") Long storeId);
 }
