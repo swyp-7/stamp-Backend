ec7659a4179f6d7e75a8d75994b05f356f12bed6
SnoopyComp <151382921+SnoopyComp@users.noreply.github.com>
2025-03-01 21:17:46 +0900

    fix: 출/퇴근 기록 조회 API의 End point 수정

--- src/main/java/com/stamp/api/attendance/controller/AttendanceController.java
+++ src/main/java/com/stamp/api/attendance/controller/AttendanceController.java
@@ -61,7 +61,7 @@ public class AttendanceController {
   }
 
   //  가게 직원들의 한달 출/퇴근 로그 조회
-  @GetMapping("/store/{storeId}/employees/attendance/month/?firstDate=YYYY-MM-DD")
+  @GetMapping("/store/{storeId}/employees/attendance/month")
   public ApplicationResponse<List<AttendanceRes>> getAttendancesForMonth(
       @PathVariable("storeId") String storeId,
       @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate firstDate,
@@ -72,7 +72,7 @@ public class AttendanceController {
   }
 
   //  가게 직원들의 하루 출/퇴근 로그 조회
-  @GetMapping("/store/{storeId}/employees/attendance/day/?firstDate=YYYY-MM-DD")
+  @GetMapping("/store/{storeId}/employees/attendance/day")
   public ApplicationResponse<List<AttendanceRes>> getAttendancesForDay(
       @PathVariable("storeId") String storeId,
       @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate firstDate,
@@ -83,7 +83,7 @@ public class AttendanceController {
   }
 
   //  특정 직원의 한달 출/퇴근 로그 조회
-  @GetMapping("/store/{storeId}/employees/attendance/month/{employeeId}/?firstDate=YYYY-MM-DD")
+  @GetMapping("/store/{storeId}/employees/attendance/month/{employeeId}")
   public ApplicationResponse<List<AttendanceRes>> getAttendancesForMonthWithEmployeeId(
       @PathVariable("storeId") String storeId,
       @PathVariable("employeeId") String employeeId,
@@ -96,7 +96,7 @@ public class AttendanceController {
   }
 
   //  특정 직원의 하루 출/퇴근 로그 조회
-  @GetMapping("/store/{storeId}/employees/attendance/day/{employeeId}/?firstDate=YYYY-MM-DD")
+  @GetMapping("/store/{storeId}/employees/attendance/day/{employeeId}")
   public ApplicationResponse<List<AttendanceRes>> getAttendancesForDayWithEmployeeId(
       @PathVariable("storeId") String storeId,
       @PathVariable("employeeId") String employeeId,
@@ -104,7 +104,7 @@ public class AttendanceController {
       @AuthenticationPrincipal UserDetails userDetails) {
 
     return ApplicationResponse.ok(
-        attendanceService.getAttendancesForMonthWithEmployeeId(
+        attendanceService.getAttendancesForDayWithEmployeeId(
             Long.valueOf(storeId), firstDate, Long.valueOf(employeeId), userDetails));
   }
 }
