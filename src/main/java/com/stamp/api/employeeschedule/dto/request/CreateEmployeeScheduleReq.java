5f761423b276f94e59e363e8f36762cfdd1e5d56
ODooHo <engh0205@naver.com>
2025-03-08 22:10:19 +0900

    fix(employeeSchedule-92): 직원 정보 수정 시 스케줄 추가가 가능하게끔 수정
    - 수정 요청 중 scheduleId가 null인 경우 새로운 스케줄 생성임으로 간주하고 스케줄을 생성

--- src/main/java/com/stamp/api/employeeschedule/dto/request/CreateEmployeeScheduleReq.java
+++ src/main/java/com/stamp/api/employeeschedule/dto/request/CreateEmployeeScheduleReq.java
@@ -4,4 +4,10 @@ import com.stamp.api.common.WeekDay;
 import java.time.LocalTime;
 
 public record CreateEmployeeScheduleReq(
-    WeekDay weekDay, LocalTime startTime, LocalTime endTime, boolean isAdditional) {}
+    WeekDay weekDay, LocalTime startTime, LocalTime endTime, boolean isAdditional) {
+
+  public static CreateEmployeeScheduleReq of(
+      WeekDay weekDay, LocalTime startTime, LocalTime endTime, boolean isAdditional) {
+    return new CreateEmployeeScheduleReq(weekDay, startTime, endTime, isAdditional);
+  }
+}
